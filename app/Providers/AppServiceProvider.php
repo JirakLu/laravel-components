214f2e44e82d61dce6786386b8b0df7611212828
JirakLu <jirak.lukas@gmail.com>
2022-08-06 11:40:58 +0200

    Added icons directive.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Blade::componentNamespace("App\\View\\Components\\Form", "form");
+        Blade::componentNamespace("App\\View\\Components\\Icons", "icons");
     }
 }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,11 +5,7 @@
         <x-form::input label="Name" name="name" corner-hint="Please enter your full name" suffix-text="cm" required>
             <span>Description</span>
             <x-slot:prefixIcon>
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                     stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                          d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"/>
-                </svg>
+                <x-icons::card-icon/>
             </x-slot:prefixIcon>
         </x-form::input>
 
